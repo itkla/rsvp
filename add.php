6c3e073ff1a63b1d8def5a4df1f55abfe979adcf
Hunter Nakagawa <hunternakagawa@gmail.com>
2019-07-10 19:57:35 -1000

    added ip capturing to prevent duplicate entries

--- add.php
+++ add.php
@@ -1,6 +1,8 @@
 <?php
   date_default_timezone_set("Pacific/Honolulu");
   $db = new SQLite3('rsvp.db');
+  // capture ip addresses to check for duplicates. NOT YET IMPLEMENTED
+  $ip = $_SERVER['REMOTE_ADDR'];
   if(isset($_POST['name'])) {
     $name = ucfirst($_POST['name']);
     $dd = $_POST['dd'];
@@ -16,7 +18,7 @@
       header("Location: ./?status=duplicate");
     } else {
       $date = date("d/m/Y h:i:sa");
-      $db->exec("INSERT INTO attendees (name, dd, time) VALUES ('$name', '$dd', '$date')");
+      $db->exec("INSERT INTO attendees (name, dd, time, ip) VALUES ('$name', '$dd', '$date', '$ip')");
       header("Location: ./?status=okay&name=$name");
     }
     /*
